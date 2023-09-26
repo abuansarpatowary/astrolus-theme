7f19132da5cd4d0eb62d80ca090556252871c45a
abuansarpatowary <abuansar040@gmail.com>
2023-09-26 15:06:05 +0600

    post loop and added body_class, post_class added

--- index.php
+++ index.php
@@ -5,37 +5,31 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body class="bg-white dark:bg-gray-800">
+<body class="bg-white dark:bg-gray-800"  <?php body_class(); ?>>
 
 <div class="py-12">
 	<div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
 		<div class="mb-12 space-y-2 text-center">
 			<h2 class="text-3xl font-bold text-gray-800 md:text-4xl dark:text-white"><?php echo bloginfo('name') ?></h2>
 			<p class="text-gray-600 dark:text-gray-300 lg:mx-auto lg:w-6/12">
-                How shall I explain here the pains and the pleasures of happy things, and the duties of pains which, in time, are due? Will you accept it with pleasure?
+                <?php echo bloginfo('description') ?>
 			</p>
 		</div>
 
 		<div class="lg:w-3/4 xl:w-2/4 lg:mx-auto">
-			<div class="group relative -mx-4 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300 hover:z-10">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="group relative -mx-4 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300 hover:z-10"  <?php post_class(); ?>>
 				<div class="sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl">
-					<img
-						src="https://images.unsplash.com/photo-1661749711934-492cd19a25c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
-						alt="art cover"
-						loading="lazy"
-						width="1000"
-						height="667"
-						class="h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105"
-					/>
+                    <?php if (has_post_thumbnail()) : ?>
+                        <?php the_post_thumbnail('medium', ['class' => 'h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105']); ?>
+                    <?php endif; ?>
 				</div>
 
 				<div class="sm:p-2 sm:pl-0 sm:w-4/6">
-					<span class="mt-4 mb-2 inline-block font-medium text-gray-400 dark:text-gray-500 sm:mt-0">Jul 27 2022</span>
-					<h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
-                        They provide that there will be a convenient flight to escape the laborious expedient.
-					</h3>
+					<span class="mt-4 mb-2 inline-block font-medium text-gray-400 dark:text-gray-500 sm:mt-0"><?php echo the_date()?></span>
+                    <?php the_title('<h3 class="text-2xl font-semibold text-gray-800 dark:text-white">', '</h3>'); ?>
 					<p class="my-6 text-gray-600 dark:text-gray-300">
-                        For those who praise him, the pleasures from which he pleases all of them, he hinders him with pains and...
+                        <?php the_content();?>
 					</p>
 
 					<div class="flex gap-4">
@@ -48,70 +42,8 @@
 					</div>
 				</div>
 			</div>
+            <?php endwhile; endif; ?>
 
-			<div class="group relative hover:z-10 -mx-4 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300">
-				<div class="sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl">
-					<img
-						src="https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
-						alt="art cover"
-						loading="lazy"
-						width="1000"
-						height="667"
-						class="h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105"
-					/>
-				</div>
-
-				<div class="sm:p-2 sm:pl-0 sm:w-4/6">
-					<span class="mt-4 mb-2 inline-block font-medium text-gray-400 dark:text-gray-500 sm:mt-0">Jul 27 2022</span>
-					<h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
-                        They provide that there will be a convenient flight to escape the laborious expedient.
-					</h3>
-					<p class="my-6 text-gray-600 dark:text-gray-300">
-                        For those who praise him, the pleasures from which he pleases all of them, he hinders him with pains and...
-					</p>
-
-					<div class="flex gap-4">
-						<a href="#" class="px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300">
-							Tailwindcss
-						</a>
-						<a href="#" class="px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300">
-							VueJS
-						</a>
-					</div>
-				</div>
-			</div>
-
-			<div class="group relative hover:z-10 -mx-4 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300">
-				<div class="sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl">
-					<img
-						src="https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
-						alt="art cover"
-						loading="lazy"
-						width="1000"
-						height="667"
-						class="h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105"
-					/>
-				</div>
-
-				<div class="sm:p-2 sm:pl-0 sm:w-4/6">
-					<span class="mt-4 mb-2 inline-block font-medium text-gray-400 dark:text-gray-500 sm:mt-0">Jul 27 2022</span>
-					<h3 class="text-2xl font-semibold text-gray-800 dark:text-white">
-                        They provide that there will be a convenient flight to escape the laborious expedient.
-					</h3>
-					<p class="my-6 text-gray-600 dark:text-gray-300">
-                        For those who praise him, the pleasures from which he pleases all of them, he hinders him with pains and...
-					</p>
-
-					<div class="flex gap-4">
-						<a href="#" class="px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300">
-							Tailwindcss
-						</a>
-						<a href="#" class="px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300">
-							VueJS
-						</a>
-					</div>
-				</div>
-			</div>
 		</div>
 	</div>
 </div>
