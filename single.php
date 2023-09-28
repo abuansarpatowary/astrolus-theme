365d0432abe77b790fa9c46e489a1fc6fa78f797
abuansarpatowary <abuansar040@gmail.com>
2023-09-28 21:31:21 +0600

    Previous and Next post added in single view

--- single.php
+++ single.php
@@ -21,9 +21,44 @@
         <p class="my-8 text-gray-600 dark:text-gray-300">
           <?php the_content();?>
         </p>
+
+        
+
       </div>
     </div>
   </div>
+  <div class="flex justify-center">
+  <!-- Previous Button -->
+  <?php
+  $previous_post = get_previous_post();
+  if (!empty($previous_post)) {
+  ?>
+    <a href="<?php echo get_permalink($previous_post->ID); ?>" class="flex items-center justify-center px-4 h-10 mr-3 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+      <svg class="w-3.5 h-3.5 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+      </svg>
+      Previous
+    </a>
+  <?php
+  }
+  ?>
+
+    <!-- Next Button -->
+    <?php
+    $next_post = get_next_post();
+    if (!empty($next_post)) {
+    ?>
+      <a href="<?php echo get_permalink($next_post->ID); ?>" class="flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        Next
+        <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+      </a>
+    <?php
+    }
+    ?>
+  </div>
+
 </div>
                                     
 <?php wp_footer(); ?>
